2cf17d7bddbe371dd33e52b1d7781f61e8d7c256
spMakumazan <sp.makumazan@gmail.com>
2023-12-13 19:17:59 +0300

    di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,11 +1,13 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class PostRepository {
 
     private final Map<Long, Post> postMap;
